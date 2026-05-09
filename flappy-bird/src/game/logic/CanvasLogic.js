4c53e75918df5bde8e33ddf3417920648ba2f835
Daniil Aksonov <aksunov1@gmail.com>
2026-05-09 15:50:39 +0200

    🐛 fix(constants): repair image blur

--- flappy-bird/src/game/logic/CanvasLogic.js
+++ flappy-bird/src/game/logic/CanvasLogic.js
@@ -6,13 +6,21 @@ class CanvasLogic {
   }
 
   resizeToViewport() {
-    const { width, height } = window.visualViewport;
+    const dpr = window.devicePixelRatio || 1;
+
+    const width = window.visualViewport.width * dpr;
+    const height = window.visualViewport.height * dpr;
+
+    this.resizeCanvas(width, height);
+  }
+
+  resizeCanvas(width, height) {
     this.canvas.height = height;
     this.canvas.width = Math.min(width, height * 0.55);
 
     if (this.canvas.width < MIN_WIDTH || this.canvas.height < MIN_HEIGHT) {
       throw new Error(
-        'Viewport size is too small for the game to run properly. Change your zoom level or use a device with a larger screen.'
+        'Viewport size is too small for the game to run properly. Change your zoom level or use a device with a larger screen.',
       );
     }
   }
